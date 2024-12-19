a4c30d61c0fd6a44710869c851b6809d05c5179c
Niels Ehlen <44569709+NEhlen@users.noreply.github.com>
2024-12-19 09:02:03 +0100

    solved day 19

--- 19/day19.py
+++ 19/day19.py
@@ -12,18 +12,22 @@ patterns = lines[1:]
 @cache
 def check_pattern(pattern: str):
     if not pattern:
-        return True
+        return 1
     checks = []
     for towel in towels:
         if pattern.startswith(towel):
             checks.append(check_pattern(pattern[len(towel) :]))
-    return any(checks)
+    return sum(checks)
 
 
-count_possible = 0
+count_possible_A = 0
+count_possible_B = 0
 for pattern in patterns:
     print(pattern)
-    count_possible += check_pattern(pattern)
+    options = check_pattern(pattern)
+    count_possible_A += options > 0
+    count_possible_B += options
 
 
-print("Possible patterns:", count_possible)
+print("Possible patterns A:", count_possible_A)
+print("Possible patterns B:", count_possible_B)
